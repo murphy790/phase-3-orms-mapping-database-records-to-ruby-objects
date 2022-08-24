1dc4c2a0e54a016dd35cfbaf8b25ed79cae724fb
Alex Mwaura <alexmwaura36@gmail.com>
2022-08-24 16:42:38 +0300

    commit tree 0c5ce6cda341f627290b5fa2b08c5a6cf79a08b9

--- lib/song.rb
+++ lib/song.rb
@@ -24,7 +24,7 @@ class Song
         album TEXT
       )
     SQL
-
+   # git@github.com:murphy790/phase-3-orms-mapping-database-records-to-ruby-objects.git
     DB[:conn].execute(sql)
   end
 
@@ -48,5 +48,32 @@ class Song
     song = Song.new(name: name, album: album)
     song.save
   end
+  def self.new_from_db(row)
+    # self.new is equivalent to Song.new
+    self.new(id: row[0], name: row[1], album: row[2])
+  end
+  def self.all
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
 
 end
